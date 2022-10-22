4df31687c2c0ae0209c0c08094cea1ed09cc1370
JeremyAndress <jeremysilvasilva@gmail.com>
2022-10-22 01:13:17 -0300

    Fix push script

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,4 +1,5 @@
 git config --global user.name "github-actions[bot]"
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git add -A && git commit -m "$PACKAGE_NAME NEW VERSION"
+git add --force dist/*
+git commit -m "$PACKAGE_NAME NEW VERSION"
 git push origin HEAD:main
